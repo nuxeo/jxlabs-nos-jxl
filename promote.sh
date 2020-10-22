3d2b22866df3a6a55cfef53978b1771d95d19ff9
Stephane Lacoin (aka nxmatic) <slacoin@nuxeo.com>
2020-10-22 20:01:01 +0200

    enabling promotion

--- promote.sh
+++ promote.sh
@@ -2,24 +2,9 @@
 
 echo "promoting the new version ${VERSION} to downstream repositories"
 
-exit 0 # do nothing for now
-
-jx step create pr regex \
-    --regex '\s+image: gcr.io/jxlabs-nos/jxl:(.*)' \
-    --version ${VERSION} \
-    --files "jenkins-x-*.yml" \
-    --repo https://github.com/nuxeo/jxlabs-nos-versions.git
-
 jx step create pr regex \
     --regex 'version: (.*)' \
     --version ${VERSION} \
-    --files docker/gcr.io/jxlabs-nos/jxl.yml \
-    --files packages/jxl.yml \
-    --files charts/jx-labs/jxl-boot.yml \
-    --repo https://github.com/nuxeo/jxlabs-nos-versions.git
-
-jx step create pr regex \
-    --regex 'JX_DEFAULT_IMAGE: gcr.io/jxlabs-nos/jxl:(.*)' \
-    --version ${VERSION} \
-    --files apps/jenkins-x/lighthouse/values.yaml.gotmpl \
+    --files docker/gcr.io/build-jx-prod/jxlabs-nos-master/jxlabs-nos-jxl.yml \
+    --files charts/jxlabs-nos/jxl-boot.yml \
     --repo https://github.com/nuxeo/jxlabs-nos-versions.git
